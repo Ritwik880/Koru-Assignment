feee99d938d2dd24c6384b3f5f7b81ecf97c416f
Ritwik880 <rittiksinha12@gmail.com>
2022-07-06 08:46:27 +0530

    minor fixes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,52 +1,8 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Immune from './Immune';
+import React from 'react'
 import Table from './Table';
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
-
-
 const Header = () => {
-    const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
     return (
         <>
             <nav className="navbar bg-dark">
@@ -55,7 +11,7 @@ const Header = () => {
                         <a className="navbar-brand" href="#">iData</a>
                         <div className="right">
                             <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                Launch demo modal
+                                Click here
                             </button>
                             <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
                                 aria-hidden="true">
@@ -68,42 +24,6 @@ const Header = () => {
                                         </div>
                                         <div className="modal-body">
                                             <Table />
-                                            {/* <Box sx={{ width: '100%', justifyContent: 'flex-end' }}>
-                                                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                                                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                                                        <Tab label="Immunisation Alerts" {...a11yProps(0)} />
-                                                        <Tab label="Lab Alerts" {...a11yProps(1)} />
-                                                        <Tab label="DI Alerts" {...a11yProps(2)} />
-                                                        <Tab label="Procedure Alerts" {...a11yProps(3)} />
-                                                        <Tab label="RX Specific Alerts" {...a11yProps(4)} />
-                                                        <Tab label="DX Specific Alerts" {...a11yProps(5)} />
-                                                        <Tab label="Patient Specific Alerts" {...a11yProps(6)} />
-
-                                                    </Tabs>
-                                                </Box>
-                                                <TabPanel value={value} index={0}>
-                                                    <Table />
-                                                </TabPanel>
-                                                <TabPanel value={value} index={1}>
-                                                    Lab Alerts
-                                                </TabPanel>
-                                                <TabPanel value={value} index={2}>
-                                                    DI Alerts
-                                                </TabPanel>
-                                                <TabPanel value={value} index={3}>
-                                                    Procedure Alerts
-                                                </TabPanel>
-                                                <TabPanel value={value} index={4}>
-                                                    RX Specific Alerts
-                                                </TabPanel>
-                                                <TabPanel value={value} index={5}>
-                                                    DX Specific Alerts
-                                                </TabPanel>
-                                                <TabPanel value={value} index={6}>
-                                                    Patient Specific Alerts
-                                                </TabPanel>
-
-                                            </Box> */}
                                         </div>
                                         <div className="modal-footer">
                                             <button type="button" className="btn btn-secondary"
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from 'react'
-
+import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
+import { AiFillDelete } from 'react-icons/ai'
 const Table = () => {
     const [items, setItems] = useState([{}]);
     const [search, setSearch] = useState('');
     const [filteredResults, setFilteredResults] = useState([]);
+    const [totalData, setTotalData] = useState();
+    const [errorMessage, setErrorMessage] = useState(false);
+
+
+    // Fetching data
     let limit = 10;
     const getData = async () => {
         const res = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`);
         const data = await res.json();
         const total = res.headers.get("x-total-count");
+        setTotalData(total)
         console.log(data);
         setItems(data);
-        // setTableData(total);
-        // setTableData(Math.ceil(total / limit));
 
     }
     useEffect(() => {
         getData()
     }, [])
 
+
+    // form onSubmit function
     const submitForm = (e) => {
         e.preventDefault();
         addData(e.target.name.value, e.target.email.value);
@@ -26,6 +33,8 @@ const Table = () => {
         e.target.email.value = "";
 
     }
+
+    // adding data with form
     const addData = async (name, email) => {
         await fetch('https://jsonplaceholder.typicode.com/users', {
             method: 'POST',
@@ -45,7 +54,7 @@ const Table = () => {
                 }
             })
             .then((data) => {
-                alert('Data stored successfully')
+                alert('Data added successfully')
                 setItems((items) => [...items, data])
             })
             .catch((err) => {
@@ -53,6 +62,8 @@ const Table = () => {
             })
 
     }
+
+    // Search data
     const searchItems = (searchValue) => {
         setSearch(searchValue)
         if (search !== '') {
@@ -65,17 +76,47 @@ const Table = () => {
             setFilteredResults(items)
         }
     }
+
+    // Delete individual row data
+    const handleDelete = async (id) => {
+        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: 'DELETE'
+        })
+            .then((res) => {
+                if (res.status !== 200) {
+                    return
+                } else {
+                    alert('Are you sure you want to delte')
+                    setItems(items.filter((item) => {
+                        return item.id !== id;
+                    }))
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
+    }
+
+    // Delete all row data
+    const deleteAll = (e) => {
+        e.preventDefault();
+        alert('This will delete all data')
+        setItems([]);
+        setErrorMessage(true)
+
+    }
     return (
         <>
             <div className="header">
-                <button className='tableBtn'>Immunisation Alerts</button>
+                <button id='firstBtn'>Immunisation Alerts</button>
                 <button className='tableBtn'>Lab Alerts</button>
                 <button className='tableBtn'>DI Alerts</button>
                 <button className='tableBtn'>Procedure Alerts</button>
                 <button className='tableBtn'>RX Specific Alerts</button>
                 <button className='tableBtn'>DX Specific Alerts</button>
                 <button className='tableBtn'>Immunisation Alerts</button>
-                <button className='tableBtn'>Patient Specific Alerts</button>
+                <button id='lastBtn'>Patient Specific Alerts</button>
             </div>
             <section className='tableData'>
                 <div className="row container">
@@ -86,12 +127,15 @@ const Table = () => {
                                 <p className='find'>Find</p>
                                 <input type="text" onChange={(e) => searchItems(e.target.value)} />
                             </div>
-                            <button className='delete'>Delete</button>
+                            <button className='delete' onClick={deleteAll}>Delete</button>
                         </div>
 
                         <table className="table">
                             <thead>
                                 <tr>
+                                    <th>
+                                        <button onClick={deleteAll} className='deleteRow'> <AiFillDelete /></button>
+                                    </th>
                                     <th scope="col">id</th>
                                     <th scope="col">Name</th>
                                     <th scope="col">Email</th>
@@ -99,11 +143,12 @@ const Table = () => {
                                 </tr>
                             </thead>
                             {search.length > 1 ? (
-                                filteredResults.map((curElem, i) => {
+                                filteredResults.map((curElem) => {
                                     const { id, name, email } = curElem;
                                     return (
                                         <tbody key={id}>
                                             <tr>
+                                                <button onClick={() => handleDelete(id)} className='deleteRow'> <AiFillDelete /></button>
                                                 <th scope="row">
                                                     {
                                                         id
@@ -117,7 +162,6 @@ const Table = () => {
                                             </tr>
 
                                         </tbody>
-
                                     )
                                 })
                             ) : (
@@ -126,6 +170,10 @@ const Table = () => {
                                     return (
                                         <tbody key={id}>
                                             <tr>
+
+                                                <button onClick={() => handleDelete(id)} className='deleteRow'> <AiFillDelete /></button>
+
+
                                                 <th scope="row">
                                                     {
                                                         id
@@ -144,18 +192,40 @@ const Table = () => {
                                 })
 
                             )}
+                            {
+                                errorMessage ?
+                                    <h4 className='errorMessage'>No Data to show</h4>
+                                    : null
+                            }
+
                         </table>
+                        <div className="pagination">
+                            <p> Total Results: {totalData}</p>
+                            <p>Page <span>
+                                {limit}</span> of {totalData}</p>
+                            <div className="paginateBtn">
+                                <button className='btns'>
+                                    <BsArrowLeft />
+
+                                </button>
+                                <button className='btns'>
+
+                                    <BsArrowRight />
+
+                                </button>
+                            </div>
+                        </div>
                     </div>
                     <div className="col-lg-5 col-md-12">
                         <div className="formPart">
                             <form action="#" onSubmit={submitForm}>
                                 <div className="mb-3 formLabel">
                                     <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
-                                    <input type="text" className="form-control" id="exampleFormControlInput1" name="name" autoComplete='off' required />
+                                    <input type="text" className="form-control inputBox" id="exampleFormControlInput1" name="name" autoComplete='off' required />
                                 </div>
                                 <div className="mb-3 formLabel">
                                     <label htmlFor="exampleFormControlInput1" className="form-label">Email</label>
-                                    <input type="text" className="form-control" id="exampleFormControlInput1" name="email" autoComplete='off' required />
+                                    <input type="text" className="form-control inputBox" id="exampleFormControlInput1" name="email" autoComplete='off' required />
                                 </div>
                                 <div className="addBtn">
                                     <button className='addChildBtn' onSubmit={submitForm}>Add</button>
